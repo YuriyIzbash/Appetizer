9a78a90149b7cb5593d9e7869fb19e7bebbd5cf6
YuriyIzbash <izbash91@gmail.com>
2024-12-19 22:07:20 +0100

    Reorgonized

---
+++ Appetizer/Utilities/Extentions/Color+Ext.swift
@@ -0,0 +1,14 @@
+//
+//  Color+Ext.swift
+//  Appetizer
+//
+//  Created by YURIY IZBASH on 19. 12. 24.
+//
+
+import SwiftUI
+import UIKit
+
+struct BrandColors {
+    static let swiftUIBrandPrimary = Color("brandPrimary")
+    static let uiKitBrandPrimary = UIColor(named: "brandPrimary")
+}
---
+++ Appetizer/Views/Cells/AppetizerListCell.swift
@@ -0,0 +1,41 @@
+//
+//  AppetizerListCell.swift
+//  Appetizer
+//
+//  Created by YURIY IZBASH on 17. 12. 24.
+//
+
+import SwiftUI
+
+struct AppetizerListCell: View {
+    
+    let appetizer: Appetizer
+    
+    var body: some View {
+        HStack {
+            AppetizerRemoteImage( urlString: appetizer.imageURL)
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+                .frame(width: 120, height: 90)
+                
+                
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text(appetizer.name)
+                    .font(.title2)
+                    .fontWeight(.medium)
+                    
+                    
+                    Text("$ \(appetizer.price, specifier: "%.2f")")
+                    .foregroundColor(.secondary)
+                    .fontWeight(.semibold)
+                    
+            }
+            .padding(.leading)
+        }
+    }
+}
+
+#Preview {
+    AppetizerListCell(appetizer: MockData.sampleAppetizer)
+}
