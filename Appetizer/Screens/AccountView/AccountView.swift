bb2652a01e6033a67cc5c24eab1b4c10d5a635d3
YuriyIzbash <izbash91@gmail.com>
2024-12-22 13:57:18 +0100

    Added Account View

--- Appetizer/Screens/AccountView/AccountView.swift
+++ Appetizer/Screens/AccountView/AccountView.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName: String = ""
+    @State private var lastName: String = ""
+    @State private var email: String = ""
+    @State private var birthDate: Date = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+    
     var body: some View {
-        Text("Account View")
+        NavigationView {
+            Form {
+                
+                Section(header: Text("Account")) {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .autocorrectionDisabled(true)
+                    DatePicker("Birth Date", selection: $birthDate, displayedComponents: .date)
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                }
+                Section(header: Text("Request")) {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                }
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+            }
+                .navigationTitle("Account")
+        }
     }
 }
 
